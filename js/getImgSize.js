00984a2dbe78db02adc4672d039f3f3bbb4246d2
wintergrace <wintergoodman13@gmail.com>
2022-12-19 11:55:25 -0500

    finishing touches

--- js/getImgSize.js
+++ js/getImgSize.js
@@ -1,3 +1,4 @@
+var timeout;
 window.onload= function getImgSize() {
   var width = window.innerWidth;
   var height = width*2/3;
@@ -9,6 +10,7 @@ window.onload= function getImgSize() {
   if(!div) div = document.getElementById("projects-page");
   var setHeightString = "height:" + height+"px";
   div.setAttribute("style",setHeightString);
+  playSize();
 };
 window.onresize = function getImgSize() {
   var width = window.innerWidth;
@@ -19,6 +21,18 @@ window.onresize = function getImgSize() {
   var div = document.getElementById("front-page");
   if(!div) div = document.getElementById("about-page");
   if(!div) div = document.getElementById("projects-page");
-  var setHeightString = "height:" + height+"px";
+  var setHeightString = "height: " + height+"px";
   div.setAttribute("style",setHeightString);
+  clearTimeout(timeout);
+  timeout = setTimeout(playSize, 100);
 };
+
+function playSize(){
+  var image = document.getElementById("game-image");
+  var playHeight = image.clientHeight;
+  var playlist = document.getElementById("playlist");
+  var playHeightString = "height: " + playHeight+"px";
+  playlist.setAttribute("style", playHeightString);
+  /*alert(playHeightString);
+  alert(playlist.clientHeight);*/
+}
